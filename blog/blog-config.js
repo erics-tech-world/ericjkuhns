0a25682c95b9287b47e7738a1b0960c8914d2668
Eric's Tech World <info@ericjkuhns.com>
2026-08-20 23:33:30 -0500

    Update blog-config.js

--- blog/blog-config.js
+++ blog/blog-config.js
@@ -1,1 +1,4 @@
-
+// Your Blogger blog address (no https://, no trailing slash).
+// Example: "ericjkuhns.blogspot.com"
+// Paste your blog address between the quotes below:
+var BLOG_ID = "ericjkuhns.blogspot.com";
