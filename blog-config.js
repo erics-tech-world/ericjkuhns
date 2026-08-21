c8da24ee8912635cc2a140dc16bcd9bfd92f8c1c
Eric's Tech World <info@ericjkuhns.com>
2026-08-20 23:38:20 -0500

    Delete blog-config.js

--- blog-config.js
+++
@@ -1,4 +0,0 @@
-// Your Blogger blog address (no https://, no trailing slash).
-// Example: "ericjkuhns.blogspot.com"
-// Paste your blog address between the quotes below:
-var BLOG_ID = "ericjkuhns.blogspot.com";
